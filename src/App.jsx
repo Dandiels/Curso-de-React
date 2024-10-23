7f1cc39ad1ffbe11959a4d292839085eaf226357
Daniel Freitas <101065507+Dandiels@users.noreply.github.com>
2024-10-23 17:13:22 -0300

    Second Commit - 23/10/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,79 @@
 import { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { v4 } from "uuid";
 
 function App()
 {
-  const [message, setMessage] = useState("Olá, mundo!");
-
-  return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={() =>
+    const [tasks, setTasks] = useState(
+    [
         {
-          setMessage("Olá, fui clicado!");
-        }}>Mudar Mensagem</button>
-    </div>
-  );
+            id: 1,
+            title: "Estudar Programação",
+            description: "Estudar programação para se tornar um desenvolvedor Full Stack",
+            isCompleted: false
+        },
+        {
+            id: 2,
+            title: "Estudar Inglês",
+            description: "Estudar inglês para se tornar um desenvolvedor Full Stack",
+            isCompleted: false
+        },
+        {
+            id: 3,
+            title: "Estudar Matemática",
+            description: "Estudar matemática para se tornar um desenvolvedor Full Stack",
+            isCompleted: false
+        }
+    ]);
+
+    function onTaskClick(taskId)
+    {
+        const newTasks = tasks.map((task) =>
+        {
+            if (task.id === taskId)
+            {
+                return { ...task, isCompleted: !task.isCompleted };
+            }
+
+            return task;
+        });
+
+        setTasks(newTasks);
+    }
+
+    function onDeleteTaskClick(taskId)
+    {
+        const newTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(newTasks);
+    }
+
+    function onAddTaskSubmit (title, description)
+    {
+        const newTask =
+        {
+            id: v4(),
+            title,
+            description,
+            isCompleted: false
+        };
+
+        setTasks([...tasks, newTask]);
+    }
+
+    return (
+      <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+        <div className="w-[500px] space-y-4">
+          <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+          <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+          <Tasks
+              tasks={tasks}
+              onTaskClick={onTaskClick}
+              onDeleteTaskClick={onDeleteTaskClick}
+          />
+        </div>
+      </div>
+    );
 }
 
 export default App;
